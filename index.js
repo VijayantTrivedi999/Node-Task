333aa73ba408fa4cc68dde373a71020c926982c8
VijayantTrivedi999 <141797522+VijayantTrivedi999@users.noreply.github.com>
2023-08-09 10:23:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const userRouter = require("./routes/userRoutes");
 const taskRouter = require("./routes/taskRoutes");
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/creativefueltask",
-{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});
-
-
-app.use("/users",userRouter);
+app.use("/user",userRouter);
 app.use("/task",taskRouter);
-
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("Hello Vijayant Trivedi");
 })
+
+mongoose.connect("mongodb://localhost:27017/creativefueltask",
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=> console.log('Connected to MongoDB'))
+.catch(error => console.log('MongoDB Connection error.',error));
+
 
 app.listen(5000,()=>{
     console.log('Server started on port number 5000');

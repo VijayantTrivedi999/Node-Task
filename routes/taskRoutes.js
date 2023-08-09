80d51ac36d156a3c8897500eb01dda90f2be794e
VijayantTrivedi999 <141797522+VijayantTrivedi999@users.noreply.github.com>
2023-08-09 10:05:38 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 const auth = require('../middleware/auth');
+const multer = require('multer');
 const taskRouter = express.Router();
+
+// Multer for image upload 
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, 'uploads/');
+    },
+    filename : (req,file,cb) => {
+        cb(null,new Date().toISOString() + '-' + file.originalname);
+    },
+});
+
+const upload = multer({ storage : storage });
 
 taskRouter.get("/",auth,getTasks);
 
-taskRouter.post("/",auth,createTask);
+taskRouter.post("/",auth,upload.single('attachment'),createTask);
 
-taskRouter.put("/:id",auth,updateTask);
+taskRouter.put("/:id",auth,upload.single('attachment'),updateTask);
 
 taskRouter.delete("/:id",auth,deleteTask);
 
